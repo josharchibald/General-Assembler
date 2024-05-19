083d632239bfcd95ec332375934680d54114763b
Steven <deltaorb637@gmail.com>
2024-05-19 02:57:07 -0700

    update caltech10 functions

--- caltech10_isa_functions.cpp
+++ caltech10_isa_functions.cpp
@@ -6,29 +6,16 @@
 #include <fstream>
 #include <bitset>
 
-#define OP_CODE_MAX_SIZE 16
 
-const size_t X_REG_VALUE = 10;
-const size_t S_REG_VALUE = 11;
+const size_t OP_CODE_FULL_SIZE = 16;
+
 size_t operator_parser(std::string op){
     if (op.empty()) {
         return 0;
     }
     std::cout << "val: " << op << std::endl;
-    // get the part of the value that contains the register designator
-    char first_char = op[0];
-    switch (first_char)
-    {
-    case 'X':
-        return X_REG_VALUE;
-    case 'S':
-        return S_REG_VALUE;
-    default:
-        // assume it is a constant 
-        return std::stoi(op);
-    }
+    return std::stoi(op);
 }
-
 void print_binary_with_spaces(uint16_t number) {
     std::bitset<16> binary(number);
 
@@ -39,75 +26,34 @@ void print_binary_with_spaces(uint16_t number) {
     }
     std::cout << std::endl;
 }
-// Most ALU operations. Format is #### #### vvvv vvvv (8 bit opcode + 8 bit 
-// immediate/offest/address)
-size_t f1(size_t op_code, std::string op1, std::string op2) {
+// Most alu, load, store, and i/o instructions. Format is #### #### vvvv vvvv 
+// (8 bit opcode + 8 bit immediate/offest/address)
+size_t f1(size_t op_code, std::string op1) {
     size_t data;
-    size_t default_op_code_bit_count = 8;
+    size_t op_code_bit_count = 8;
     size_t op1_toi = operator_parser(op1);
-    data = op_code << (OP_CODE_MAX_SIZE - default_op_code_bit_count);
+    // shift the op_code such that the first op_code_bit_count bits are the code
+    data = op_code << (OP_CODE_FULL_SIZE - op_code_bit_count);
     data = data | op1_toi;
     return data;
-}
-// Load/Store instructions. Format is #### #### vvvv vvvv
-size_t f2(size_t op_code, std::string op1, std::string op2) {
-    size_t op1_toi = operator_parser(op1);
-    size_t data;
-    size_t default_op_code_bit_count = 8;
-    size_t op1_toi = operator_parser(op1);
-    data = op_code << (OP_CODE_MAX_SIZE - default_op_code_bit_count);
-    data = data | op1_toi;
-    return data;
-}
-
-
-// Jump instrctions. Aside from JMP, formatting is #### #### rrrr rrrr 
-size_t f3(size_t op_code, std::string op1, std::string op2){
-    size_t data;
-    size_t default_op_code_bit_count = 8;
-    size_t jmp_code_bit_count = 3;
-    size_t op1_toi = operator_parser(op1);
-    switch(op_code)
-    {   
-        // JMP is 110a aaaa aaaa aaaaa
-        case(6):
-            data = op_code << (OP_CODE_MAX_SIZE - jmp_code_bit_count);
-            data = data | op1_toi;
-            return data;
-        default:
-            data = op_code << (OP_CODE_MAX_SIZE - default_op_code_bit_count);
-            data = data | op1_toi;
-            return data;
-    }
 }
 // Opcode is directly represented. Applies for stack operations, NOP, Index 
-// Register instructions, Flag instructions, and some ALU operations
-size_t f4(size_t op_code, std::string op1, std::string op2){
+// Register instructions, Flag instructions, and some ALU operations, NOP
+size_t f2(size_t op_code) {
     return op_code;
 }
-// Call instruction, 111a aaaa aaaa aaaa
-size_t f5(size_t op_code, std::string op1, std::string op2){
+
+// JMP and Call instructions. Format is ###a aaaa aaaa aaaa
+size_t f3(size_t op_code, std::string op1){
     size_t data;
-    size_t default_op_code_bit_count = 3;
+    size_t op_code_bit_count = 3;
     size_t op1_toi = operator_parser(op1);
-    data = op_code << (OP_CODE_MAX_SIZE - default_op_code_bit_count);
+    // shift the op_code such that the first op_code_bit_count bits are the code
+    data = op_code << (OP_CODE_FULL_SIZE - op_code_bit_count);
     data = data | op1_toi;
     return data;
 }
 
-// Other stack instructions: RTS, POPF, PUSHF.  Format is all 16 bits.
-size_t f6(size_t op_code, std::string op1, std::string op2){    
-    return op_code;
-}
-// I/O instructions. Format is #### #### pppp pppp.
-size_t f7(size_t op_code, std::string op1, std::string op2){
-    size_t data;
-    size_t default_op_code_bit_count = 8;
-    size_t op1_toi = operator_parser(op1);
-    data = op_code << (OP_CODE_MAX_SIZE - default_op_code_bit_count);
-    data = data | op1_toi;
-    return data;
-}
 
 // int main() {
 //     std::string rd = "ZL";
